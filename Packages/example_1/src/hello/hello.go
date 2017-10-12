91ca55dd1e4858d5a8aac60dabd27c79543afb35
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-10-13 00:24:39 +0800

    chore: Add a whole functions that is to be tested and run

--- Packages/example_1/src/hello/hello.go
+++ Packages/example_1/src/hello/hello.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main(){
-  fmt.Println("cacaca")
-  miao.Miao()
+	fmt.Println("cacaca")
+	miao.Miao()
+
+	r := miao.Rect{Width: 3, Height: 4}
+	c := miao.Circle{Radius: 5}
+
+	miao.Measure(r)
+	miao.Measure(c)
+	miao.Heyza(r)
+	miao.Heyza(c)
+	miao.Zazzas()
 }
