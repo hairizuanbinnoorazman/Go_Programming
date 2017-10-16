caf098ca3b62b8d022c7b96c851da9ae31d27c1c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-10-16 10:23:02 +0800

    feat: Add a go application that deal with slack

---
+++ Apps/slack_example.go
@@ -0,0 +1,46 @@
+// Example of using Go to ping Slack
+// This would ping a message by the text message passed via postMessage function
+
+package main
+
+import (
+	"fmt"
+	
+	"encoding/json"
+	"bytes"
+	"io/ioutil"
+
+	"net/http"
+)
+
+type message struct {
+	Text string    `json:"text"`
+}
+
+func postMessage(msg string) {
+	slackUrl := "https://hooks.slack.com/services/<<KEYS>>"
+
+	// Create a reader to be used by http.Post
+	response := message{Text: msg}
+	body, _ := json.Marshal(response)
+	byteBody := bytes.NewReader(body)
+
+	res, err := http.Post(slackUrl, "application/json", byteBody)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Try again later.")
+	}
+
+	fmt.Println("Status of response:", res.Status)
+	fmt.Println("Status code of response:", res.StatusCode)
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(content))
+}
+
+func main() {
+	fmt.Println("A test application to fire a message into Slack")
+	postMessage("init")
+}
