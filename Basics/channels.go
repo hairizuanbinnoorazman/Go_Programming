9b7ec1ae1f1fd5304e01b80bd91e346fd51eb7fb
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-10-20 09:49:44 +0800

    chore: Do quick check - on how to determine length of channel -> It can only be determined if its a buffered channel (must have length specified); if not, the whole thing wont work as expected

--- Basics/channels.go
+++ Basics/channels.go
@@ -6,6 +6,8 @@
 
 	In order to make a random sleeper -> Convert to type accordingly.
 	Convert int to Duration and then multiply it to the time.Second to properly get the time value
+
+	There is no length property for unbuffered channels. Only if you add buffer to it will you be able to get the length out.
 */
 
 package main
@@ -17,25 +19,32 @@ import (
 	"math/rand"
 )
 
-func randomFire(c chan int) {
-	log.Println("Random Fire Function Started")
-	time.Sleep(time.Duration(rand.Intn(5) + 1) * time.Second)
-	randomNumber := rand.Intn(1000)
-	fmt.Println("Random Fire Function Generated the following number:", randomNumber)
-	c <- randomNumber
+
+func randomFireConsumer(c chan int, worker int) {
+	for {
+		log.Println("Random Fire Function Started for worker", worker)
+		time.Sleep(time.Duration(rand.Intn(5) + 1) * time.Second)
+		fmt.Println("Random Fire Function Generated the following number:", <- c)
+	}
 }
 
-func randomFireLoop(c chan int) {
+func randomFireGenerator(c chan int) {
 	for {
-		time.Sleep(2 * time.Second)
-		go randomFire(c)
+		time.Sleep(100 * time.Millisecond)
+		c <- rand.Intn(1000)
 	}
 }
 
 func main() {
-	a := make(chan int)
-	go randomFireLoop(a)
+	a := make(chan int, 200)
+	go randomFireGenerator(a)
+
+	for i := 0; i < 6; i++ {
+		go randomFireConsumer(a, i)
+	}
+
 	for {
-		fmt.Println(<- a)
+		time.Sleep(5 * time.Second)
+		fmt.Println("Number of i terms in the channel currently:", len(a))
 	}
 }
