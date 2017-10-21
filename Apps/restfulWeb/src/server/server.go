c6cab3ba1931f23587fd96063dde433cdf0b1cc9
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-10-22 02:42:19 +0800

    feat: Add the list and delete endpoints - part of restful design

--- Apps/restfulWeb/src/server/server.go
+++ Apps/restfulWeb/src/server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
@@ -43,15 +44,97 @@ func (h insertPersonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Error miao", err.Error())
 	}
-	w.WriteHeader(201)
+	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(replyData)
 }
 
+type getPersonHandler struct{}
+
+func (h getPersonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	log.Println(id)
+	log.Println(len(people))
+
+	var person Person
+	for _, value := range people {
+		log.Println(value.ID)
+		log.Println(value.Address.City)
+		log.Println(value.ID == string(id))
+		if value.ID == id {
+			person = value
+			encoder := json.NewEncoder(w)
+			encoder.Encode(person)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprint(w)
+}
+
+func router() *mux.Router {
+	// Reassign value for people variable
+	people = people[:0]
+	people = append(people, Person{"999", "miao", "miao", &Address{"miao", "mia"}})
+
+	// Set up router
+	router := mux.NewRouter()
+	router.Handle("/people/", insertPersonHandler{}).Methods("POST")
+	router.Handle("/people/", getPeopleHandler{}).Methods("GET")
+	router.Handle("/people/{id}/", getPersonHandler{}).Methods("GET")
+	router.Handle("/people/{id}", deletePeopleHandler{}).Methods("DELETE")
+	return router
+}
+
+type getPeopleHandler struct{}
+
+func (h getPeopleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(people)
+}
+
+type deletePeopleHandler struct{}
+
+func (h deletePeopleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Since this is a poor implementation of stored data, there might be duplicates
+	// We will be deleting all instances of records that contains 999
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	beforeLength := len(people)
+
+	var tempPeople []Person
+
+	for _, value := range people {
+		if value.ID != id {
+			tempPeople = append(tempPeople, value)
+		}
+	}
+
+	people = tempPeople
+	afterLength := len(people)
+
+	type deleteResponse struct {
+		Id           string `json:"id"`
+		DeleteStatus string `json:"delete_status"`
+	}
+
+	deleteStatus := "failed"
+	if beforeLength > afterLength {
+		deleteStatus = "success"
+	}
+	response := deleteResponse{Id: id, DeleteStatus: deleteStatus}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(response)
+}
+
 func main() {
 	log.Println("server started")
-	router := mux.NewRouter()
-	router.Handle("/people", insertPersonHandler{}).Methods("POST")
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", router))
+	log.Println("Create fake data")
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", router()))
 }
--- Apps/restfulWeb/src/server/server_test.go
+++ Apps/restfulWeb/src/server/server_test.go
@@ -6,9 +6,9 @@ Endpoints to consider
 - Get single person
 - Get a list of people
 - Post Create
-- Patch Update
-- Put Update
 - Delete person
+- Patch person (TODO)
+- Put person (TODO)
 
 Important point to take note:
 - Ensure that items in struct is capitalized - if not capitalized, will have issues to convert between json and struct
@@ -30,16 +30,17 @@ import (
 
 func TestCreate(t *testing.T) {
 	cases := []Person{
-		{"1", "Ann", "Mcdonald", &Address{"Singapore", "Singapore"}},
+		{"1000", "Ann", "Mcdonald", &Address{"Singapore", "Singapore"}},
 	}
 
-	server := httptest.NewServer(insertPersonHandler{})
+	server := httptest.NewServer(router())
+	defer server.Close()
 	for idx, person := range cases {
 		data, err := json.Marshal(person)
 		if err != nil {
 			t.Fatal("Unable to process cases properly. Please inspect accordingly")
 		}
-		res, err := http.Post(server.URL, "application/json", bytes.NewReader(data))
+		res, err := http.Post(server.URL+"/people/", "application/json", bytes.NewReader(data))
 		if err != nil {
 			t.Error("Unable to add the following case", idx, "Error:", err.Error())
 		}
@@ -68,5 +69,105 @@ func TestCreate(t *testing.T) {
 }
 
 func TestGet(t *testing.T) {
+	server := httptest.NewServer(router())
+	defer server.Close()
+
+	data, err := http.Get(server.URL + "/people/999/")
+	t.Log(server.URL + "/people/999/")
+	if err != nil {
+		t.Error("Server endpoint not available.", err.Error())
+	}
+
+	dataRaw, err := ioutil.ReadAll(data.Body)
+	if err != nil {
+		t.Error("Unexpected error in trying to get body of the data", err.Error())
+	}
+
+	var response Person
+	json.Unmarshal(dataRaw, &response)
+
+	if response.ID != "999" {
+		t.Error("Expected 999. Received", response.ID)
+	}
+	if response.Lastname != "miao" {
+		t.Error("Expected miao. Received", response.Lastname)
+	}
+	if response.Address.City != "miao" {
+		t.Error("Expected miao. Received", response.Address.City)
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	server := httptest.NewServer(router())
+	defer server.Close()
+
+	res, err := http.Get(server.URL + "/people/")
+	if err != nil {
+		t.Fatal("Unable to hit the people endpoint")
+	}
+
+	if res.StatusCode != 200 {
+		t.Error("Wrong error status")
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal("Unable to read the response from people endpoint")
+	}
+
+	var jsonData []Person
+	json.Unmarshal(data, &jsonData)
+
+	if len(jsonData) != 1 {
+		t.Error("Expected length of data to be returned to be 1 got:", len(jsonData))
+	}
+	if len(jsonData) > 0 {
+		if jsonData[0].ID != "999" {
+			t.Error("Expected ID: 999 Received:", jsonData[0].ID)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	cases := []string{
+		"999",
+	}
+
+	server := httptest.NewServer(router())
+	defer server.Close()
+
+	client := &http.Client{}
+
+	for _, value := range cases {
+		req, err := http.NewRequest(http.MethodDelete, server.URL+"/people/"+value, nil)
+		if err != nil {
+			t.Fatal("Unable to hit endpoint")
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			t.Fatal("Unable to hit delete endpoint")
+		}
+		data, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal("Unable to parse data from the delete endpoint")
+		}
+
+		type deleteResponse struct {
+			Id           string `json:"id"`
+			DeleteStatus string `json:"delete_status"`
+		}
+
+		var status deleteResponse
+		err = json.Unmarshal(data, &status)
+		if err != nil {
+			t.Fatal("Unable to parse data from delete endpoint")
+		}
+		if status.Id != value {
+			t.Error("Expected Deleted ID:", value, "Received:", status.Id)
+		}
+		if status.DeleteStatus != "success" {
+			t.Error("Expected DeleteStatus: success Received:", status.DeleteStatus)
+		}
+	}
 
 }
