4636344621ad70999a2cb0d89b35720d8500b97b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-10-31 10:28:06 +0800

    chore: Add in gokit experimentation

---
+++ Apps/gokitExample1/src/server/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+type StringService interface {
+	Uppercase(context.Context, string) (string, error)
+	Count(context.Context, string) int
+}
+
+var ErrEmpty = errors.New("Empty String")
+
+type stringService struct{}
+
+func (stringService) Uppercase(_ context.Context, s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(_ context.Context, s string) int {
+	return len(s)
+}
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V string `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
