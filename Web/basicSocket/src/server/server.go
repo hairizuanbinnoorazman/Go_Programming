cc20d831d137a4bf0790d81745e7114117ec9972
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-11-09 14:24:02 +0800

    chore: Put in the source code to handle websockets

---
+++ Web/basicSocket/src/server/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+	"log"
+	"time"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	log.Println("Start Server")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request){
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn){
+			for {
+				mType, msg, err := conn.ReadMessage()
+				if err != nil {
+					log.Println(err.Error())
+				}
+
+				log.Println(string(msg))
+				conn.WriteMessage(mType, msg)
+			}
+		}(conn)
+	})
+
+	http.HandleFunc("/v2/ws", func(w http.ResponseWriter, r *http.Request){
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func (conn *websocket.Conn){
+			ch := time.Tick(time.Second)
+
+			for range ch {
+				conn.WriteJSON(myStruct{Username:"cnkamclacmajnejra"})
+			}
+		}(conn)
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+type myStruct struct {
+	Username string `json:"username"`
+}
