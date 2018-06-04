383fbbb201d0869b43e32da443f365c34bd06842
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-06-04 22:43:20 +0800

    feat: Add skeleton bst

---
+++ Algo/DataStructures/BinarySearchTree.go
@@ -0,0 +1,12 @@
+/*
+Binary search tree data structure
+
+- Left node has data that is less that the node's key
+- Right node has data that is more than the node's key
+- All must be a single binary tree all the way down
+*/
+
+package main
+
+func main() {
+}
