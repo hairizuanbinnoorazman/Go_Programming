fdf088a0365bd0f52482adab3075c343308f1977
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-05-28 09:00:07 +0800

    moved and use it as part of demo

--- Web/basicWeb/main.go
+++ Web/basicWeb/main.go
@@ -1,15 +1,15 @@
 /*
-Basic web server implementation in go. 
+Basic web server implementation in go.
 To run this file run the following command:
 
 go run basic_http_web.go
 
 Some notes to think about
-- Difference between Printf and Fprint: Printf prints a bunch of stuff into stdout. 
+- Difference between Printf and Fprint: Printf prints a bunch of stuff into stdout.
   Fprint prints a bunch of stuff into a file/writer
   Sprintf returns a string
   Printf writes the stuff into stdout and returns bytes return and any write error
-- Printf function to handle values can use %v. It replaces it with values. It can take on most values but -> 
+- Printf function to handle values can use %v. It replaces it with values. It can take on most values but ->
   It will convert to string first though
 - You feed a handler function for the endpoint specified in the go file
 */
@@ -25,13 +25,13 @@ import (
 func main() {
 	port := 8080
 
-	http.HandleFunc("/helloworld", helloWorldHandler)
+	http.HandleFunc("/", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request){
-	log.Println("serving", req.URL)
-	fmt.Fprint(w, "Hello World!!\n")
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving", r.URL)
+	fmt.Fprint(w, "Hello World v3!!\n")
 }
