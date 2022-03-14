cb0b3bb680c2bbb500636e3d307424cd35c7ca0e
Hairizuan <hairizuanbinnoorazman@gmail.com>
2022-03-14 22:57:50 +0800

    updated fibonacci problem

--- Algo/Mathematical/Fibonacci/fibonacci.go
+++ Algo/Mathematical/Fibonacci/fibonacci.go
@@ -3,17 +3,16 @@ package main
 import "fmt"
 
 func fibonacci(n int) int {
-	//fmt.Println(n)
-	if n > 1 {
-		value := fibonacci(n-1) + fibonacci(n-2)
-		return value
+	if n <= 0 {
+		return 0
 	}
-	if n == 1 {
+	if n == 1 || n == 2 {
 		return 1
 	}
-	return 0
+	return fibonacci(n-1) + fibonacci(n-2)
 }
 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21,...
 func main() {
-	fmt.Println(fibonacci(10))
+	fmt.Println(fibonacci(8))
 }
