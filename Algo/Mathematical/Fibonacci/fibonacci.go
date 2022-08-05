1b6374cfd3d66424bf9c79d39d1dd9d2559b0a31
Hairizuan <hairizuanbinnoorazman@gmail.com>
2022-08-05 21:28:33 +0800

    added fibonacci example

--- Algo/Mathematical/Fibonacci/fibonacci.go
+++ Algo/Mathematical/Fibonacci/fibonacci.go
@@ -2,17 +2,43 @@ package main
 
 import "fmt"
 
+var store = map[int]int{
+	1: 1,
+	2: 1,
+}
+
 func fibonacci(n int) int {
 	if n <= 0 {
 		return 0
 	}
-	if n == 1 || n == 2 {
+	if store[n] != 0 {
+		return store[n]
+	}
+	val := fibonacci(n-1) + fibonacci(n-2)
+	store[n] = val
+	return val
+}
+
+func fibonacciTabulate(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	if n <= 2 {
 		return 1
 	}
-	return fibonacci(n-1) + fibonacci(n-2)
+	previous1 := 1
+	previous2 := 1
+	currentVal := 0
+	for i := 3; i <= n; i++ {
+		currentVal = previous1 + previous2
+		previous1 = previous2
+		previous2 = currentVal
+	}
+	return currentVal
 }
 
 // 0, 1, 1, 2, 3, 5, 8, 13, 21,...
 func main() {
-	fmt.Println(fibonacci(8))
+	fmt.Println(fibonacci(100))
+	fmt.Println(fibonacciTabulate(100))
 }
