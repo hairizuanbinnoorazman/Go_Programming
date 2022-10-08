154b975de0ad1b57587ffefe00c86896f2b7bb72
Hairizuan <hairizuanbinnoorazman@gmail.com>
2022-10-09 03:08:50 +0800

    updated to show chart

--- Web/makeCharts/main.go
+++ Web/makeCharts/main.go
@@ -3,28 +3,81 @@ package main
 import (
 	"context"
 	"embed"
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/chromedp/chromedp"
+	"github.com/gorilla/mux"
 )
 
 //go:embed static/*
 var content embed.FS
 
-func main() {
-	http.Handle("/screenshot", &ScreenshotIt{})
-	http.Handle("/struct", &HandleViaStruct{})
-	http.ListenAndServe(":8080", nil)
+type data struct {
+	Title      string   `json:"title"`
+	XaxisTitle string   `json:"x_axis_title"`
+	Labels     []string `json:"labels"`
+	Data       []int    `json:"data"`
 }
 
-type ScreenshotIt struct{}
+func main() {
+	bucketName := os.Getenv("BUCKET_NAME")
 
-func (*ScreenshotIt) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+	r.Handle("/screenshot", &ScreenshotIt{
+		BucketName: bucketName,
+	}).Methods("POST")
+	r.Handle("/struct", &HandleViaStruct{}).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:8080",
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+type ScreenshotIt struct {
+	BucketName string
+}
+
+func (s *ScreenshotIt) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("start screenshot")
 	defer log.Println("end screenshot")
+	raw, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write([]byte("bad response"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var d data
+	json.Unmarshal(raw, &d)
+
+	u, _ := url.Parse("http://localhost:8080/struct")
+	values := u.Query()
+	values.Set("title", d.Title)
+	values.Set("x_axis_title", d.XaxisTitle)
+	values.Set("labels", strings.Join(d.Labels, ","))
+	var convertedData []string
+	for _, v := range d.Data {
+		convertedData = append(convertedData, strconv.Itoa(v))
+	}
+	values.Set("data", strings.Join(convertedData, ","))
+	u.RawQuery = values.Encode()
+	structURL := u.String()
+	log.Println(structURL)
+
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
 		// chromedp.WithDebugf(log.Printf),
@@ -33,14 +86,64 @@ func (*ScreenshotIt) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	var buf []byte
 	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, fullScreenshot(`http://localhost:8080/struct`, 90, &buf)); err != nil {
+	if err := chromedp.Run(ctx, fullScreenshot(structURL, 90, &buf)); err != nil {
 		log.Fatal(err)
 	}
-	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+	fileName := strings.ReplaceAll(d.Title+".png", " ", "_")
+	if err := ioutil.WriteFile(fileName, buf, 0o644); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
+	log.Println("screenshot file created")
+	err = uploadFile(s.BucketName, fileName, fileName)
+	if err != nil {
+		w.Write([]byte("failed to store chart"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	type resp struct {
+		URL string `json:"url"`
+	}
+
+	outputResp := resp{URL: "https://storage.googleapis.com/" + s.BucketName + "/" + fileName}
+	outputRaw, _ := json.Marshal(outputResp)
+
+	w.Write(outputRaw)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func uploadFile(bucket, object, filename string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("os.Open: %v", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	o := client.Bucket(bucket).Object(object)
+	wc := o.NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	acl := client.Bucket(bucket).Object(object).ACL()
+	if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return fmt.Errorf("ACLHandle.Set: %v", err)
+	}
+	return nil
 }
 
 type HandleViaStruct struct{}
@@ -48,15 +151,21 @@ type HandleViaStruct struct{}
 func (*HandleViaStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	defer log.Print("End hello world request")
-	type data struct {
-		Title      string
-		XaxisTitle string
-		Labels     []string
-		Data       []int
+	var d data
+	d.Title = r.URL.Query().Get("title")
+	d.XaxisTitle = r.URL.Query().Get("x_axis_title")
+	d.Labels = strings.Split(r.URL.Query().Get("labels"), ",")
+	strData := strings.Split(r.URL.Query().Get("data"), ",")
+
+	var intData []int
+	for _, v := range strData {
+		vv, _ := strconv.Atoi(v)
+		intData = append(intData, vv)
 	}
+	d.Data = intData
 
 	tmpl := template.Must(template.ParseFS(content, "static/chart.html"))
-	tmpl.Execute(w, data{Title: "Sales Figures", XaxisTitle: "Months", Labels: []string{"Jan", "Feb"}, Data: []int{10, 20}})
+	tmpl.Execute(w, d)
 }
 
 func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
