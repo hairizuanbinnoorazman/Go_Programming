316ec6f34b585cd567d9c76c18795fcb48aba4dd
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-07-18 03:50:26 +0800

    added dockerfile and main app

--- Apps/googlecompute/go.mod
+++ Apps/googlecompute/go.mod
@@ -2,4 +2,8 @@ module github.com/hairizuanbinnoorazman/hahax
 
 go 1.14
 
-require google.golang.org/api v0.22.0
+require (
+	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	google.golang.org/api v0.126.0
+)
--- Apps/googlecompute/main.go
+++ Apps/googlecompute/main.go
@@ -3,23 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	compute "google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
 )
 
-var GCP_PROJECT_ID = "XXXX"
-
 func main() {
-	// Added comment
-	fmt.Println("Test")
+	fmt.Println("Starting application")
+
+	SERVICE_ACCOUNT_FILE := os.Getenv("SERVICE_ACCOUNT_FILE")
+	GCP_PROJECT_ID := os.Getenv("GCP_PROJECT_ID")
+	REGION := os.Getenv("REGION")
+	fmt.Printf("Print GCP_PROJECT_ID: %v\n", GCP_PROJECT_ID)
+
 	ctx := context.Background()
-	computeService, err := compute.NewService(ctx)
+	var computeService *compute.Service
+	var err error
+	if SERVICE_ACCOUNT_FILE != "" {
+		fmt.Println("create computeservice with credentials file")
+		computeService, err = compute.NewService(ctx, option.WithCredentialsFile(SERVICE_ACCOUNT_FILE))
+	} else {
+		fmt.Println("create computeservice with workload identity")
+		computeService, err = compute.NewService(ctx)
+	}
 	if err != nil {
 		panic("Unable to create compute service")
 	}
 	for {
-		request := computeService.Instances.List(GCP_PROJECT_ID, "us-central1-a")
+		request := computeService.Instances.List(GCP_PROJECT_ID, REGION)
 		zz, err := request.Do()
 		if err != nil {
 			fmt.Printf("Error: %s", err.Error())
