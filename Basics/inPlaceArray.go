33f7e002a04cf45b24a6de5d2d5a1c8272bbf097
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-10-14 22:09:33 +0800

    added in place array golang code

---
+++ Basics/inPlaceArray.go
@@ -0,0 +1,44 @@
+/*
+For practise - need to be able to remove and add elements to an array in place with no issue
+
+Problems:
+1. Given an array - remove an item from it via index, and then move the rest of the elements down
+2. Given an array - move one number to the right hand side of the array
+*/
+package main
+
+import "fmt"
+
+func main() {
+	items := []int{1, 2, 3, 4, 5}
+	removeElements(items, 3, 5)
+	fmt.Println(items)
+	removeElements(items, 2, 4)
+	fmt.Println(items)
+	moveVal(items, 2)
+	fmt.Println(items)
+}
+
+func removeElements(a []int, idx, actualLen int) {
+	for i := idx; i < actualLen-1; i++ {
+		a[idx] = a[idx+1]
+	}
+	a[actualLen-1] = -1
+}
+
+func moveVal(a []int, val int) {
+	sortHappened := true
+	for sortHappened {
+		sortHappened = false
+		for k, _ := range a {
+			if k == len(a)-1 {
+				continue
+			}
+			if a[k] == val {
+				a[k] = a[k+1]
+				a[k+1] = val
+				sortHappened = true
+			}
+		}
+	}
+}
