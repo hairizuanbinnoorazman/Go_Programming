37ec3620177aeeca38e6d2d2e6be1a30930383fa
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-10-25 00:28:38 +0800

    added gke deployment module

---
+++ Web/basic/deployment/terraform/modules/gke/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.3.0"
+    }
+
+    kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "~> 2.23.0"
+    }
+  }
+}
+
+provider "google" {
+    project = "${var.gcp_project_id}"
+    region  = "${var.gcp_region}"
+    zone    = "${var.gcp_zone}"
+}
+
+resource "google_container_cluster" "test_cluster" {
+    name             = "${var.cluster_name}"
+    enable_autopilot = true
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/using_gke_with_terraform
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "test_cluster" {
+    name     = "${var.cluster_name}"
+}
+
+provider "kubernetes" {
+    host  = "https://${data.google_container_cluster.test_cluster.endpoint}"
+    token = data.google_client_config.provider.access_token
+    cluster_ca_certificate = base64decode(
+        data.google_container_cluster.test_cluster.master_auth[0].cluster_ca_certificate,
+    )
+}
+
+resource "kubernetes_deployment_v1" "basic_app_deployment" {
+    metadata {
+        name = "basic-app" 
+    }
+    spec {
+        replicas = 2
+        template {
+            metadata {
+                labels = {
+                    app = "basic-app"
+                }
+            }
+            spec {
+                container {
+                    image = "gcr.io/${var.gcp_project_id}/${var.image_name}:${var.image_tag}"
+                    name  = "basic-app"
+                    resources {
+                        limits = {
+                            cpu    = "1"
+                            memory = "512Mi"
+                        }
+                        requests = {
+                            cpu    = "0.5"
+                            memory = "256Mi"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Web/basic/deployment/terraform/modules/gke/variable.tf
@@ -0,0 +1,34 @@
+variable "gcp_project_id" {
+    description = "Define the GCP Project ID that we will interacting with"
+    type        = string
+}
+
+variable "gcp_region" {
+    description = "Region to deploy the zone to"
+    type        = string
+    default     = "us-east1"
+}
+
+variable "gcp_zone" {
+    description = "Zone to deploy the cluster to"
+    type        = string
+    default     = "us-east1-a"
+}
+
+variable "cluster_name" {
+    description = "Name of cluster to deploy"
+    type        = string
+    default     = "cluster-1"
+}
+
+variable "image_name" {
+    description = "Name of image to be deployed"
+    type        = string
+    default     = "basic-app"
+}
+
+variable "image_tag" {
+    description = "Tag of image to be deployed"
+    type        = string
+    default     = "v1"
+}
