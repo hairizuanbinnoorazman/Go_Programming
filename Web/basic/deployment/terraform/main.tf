a2f7bb990d259284578fd607b3a18e4035a27670
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-10-25 00:28:16 +0800

    added new module to manage gke deployments

--- Web/basic/deployment/terraform/main.tf
+++ Web/basic/deployment/terraform/main.tf
@@ -4,3 +4,9 @@ module "localdocker" {
     image_name = "${var.image_name}"
     image_tag  = "${var.image_tag}"
 }
+
+module "gcp_kubernetes" {
+    source = "./modules/gke"
+
+    gcp_project_id = "${var.gcp_project_id}"
+}
