b56b587f73b02ea956be98927ff91672fe9a75e7
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-11-15 08:05:37 +0800

    feat: Added sample golang code for demo-ing feature controlled by environment variables

---
+++ Web/featureflag/cmd/environ/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+var (
+	tpl = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Environment Controlled</title>
+	</head>
+	<body>
+		<h1>{{ .MainTitle }}</h1>
+
+	</body>
+</html>`
+)
+
+func main() {
+	log.Println("begin server")
+	exampleTitle := os.Getenv("TITLE")
+
+	http.Handle("/", &index{Title: exampleTitle})
+	s := &http.Server{
+		Addr: "0.0.0.0:8080",
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
+type index struct {
+	Title string
+}
+
+func (i *index) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("start index handler")
+	defer log.Println("end index handler")
+
+	t, err := template.New("webpage").Parse(tpl)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to parse"))
+		return
+	}
+
+	data := struct {
+		MainTitle string
+	}{
+		MainTitle: i.Title,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	err = t.Execute(w, data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to parse"))
+		return
+	}
+}
