fd6dd009786806dabcf5a1339b842f809477eca3
Hairizuan <hairizuanbinnoorazman@gmail.com>
2023-12-28 22:49:00 +0800

    fix: Added more files for adding jenkins jobs automatically to jenkins

---
+++ Environment/jenkins/jobs/firstjob.groovy
@@ -0,0 +1,13 @@
+String fileContents = new File('/home/pipelines/firstjob.Jenkinsfile').text
+pipelineJob("firstjob") {
+    parameters {
+        stringParam('name', "", 'name of the person')
+    }
+    definition {
+        cps {
+            script(fileContents)
+            sandbox()
+        }
+    }
+    
+}
