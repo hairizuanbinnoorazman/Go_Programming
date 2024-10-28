c51d17cdbf20300e8e3c30f7cd5346aa26acb6e0
Hairizuan <hairizuanbinnoorazman@gmail.com>
2024-10-29 01:33:19 +0800

    feat: Added handler for various endpoints

--- Apps/code-executor-k8s/main.go
+++ Apps/code-executor-k8s/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"bytes"
 	"context"
+	"embed"
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/mux"
 	batchv1 "k8s.io/api/batch/v1"
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -40,64 +47,160 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	c := codeHandler{
+	_ = codeHandler{
 		client:           clientset,
 		workingNamespace: "default",
 	}
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		// pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
-		// if err != nil {
-		// 	panic(err.Error())
-		// }
-		// fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	// for {
+	// 	c.deleteJob("test-test")
+	// 	c.deleteConfigmap("test-test")
+	// 	c.createCodeConfigmap("test-test", "import time\ntime.sleep(30)\nprint(\"test\")")
+	// 	c.createJob("test-test", "test-test")
+	// 	podName, err := c.getPodName("zzz=zzz")
+	// 	if err != nil {
+	// 		fmt.Printf("require further investigation: %v\n", err)
+	// 		continue
+	// 	}
+	// 	yahoo := c.getPodLogs(podName)
+	// 	fmt.Printf("Logs from pod: %v\n", yahoo)
+	// }
+	items := make(map[string]codeRecord)
+	zzz := cdb{Items: items}
+	r := mux.NewRouter()
 
-		// podLogOpts := core.PodLogOptions{
-		// 	Container: "alertmanager",
-		// }
-		// req := clientset.CoreV1().Pods("default").GetLogs("alertmanager-monitoring-kube-prometheus-alertmanager-0", &podLogOpts)
-		// zz, err := req.Stream(context.TODO())
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// defer zz.Close()
-
-		// buf := new(bytes.Buffer)
-		// _, err = io.Copy(buf, zz)
-		// str := buf.String()
-		// fmt.Printf("Print logs for %v\n", "alertmanager-monitoring-kube-prometheus-alertmanager-0")
-		// fmt.Println(str)
-		// fmt.Printf("End collecting logs for %v\n", "alertmanager-monitoring-kube-prometheus-alertmanager-0")
-
-		c.deleteJob("test-test")
-		c.deleteConfigmap("test-test")
-		c.createCodeConfigmap()
-		c.createJob()
-		podName, err := c.getPodName("zzz=zzz")
-		if err != nil {
-			fmt.Printf("require further investigation: %v\n", err)
-			continue
-		}
-		yahoo := c.getPodLogs(podName)
-		fmt.Printf("Logs from pod: %v\n", yahoo)
-
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		// _, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		// if errors.IsNotFound(err) {
-		// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		// } else if err != nil {
-		// 	panic(err.Error())
-		// } else {
-		// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
-		// }
-
-		time.Sleep(10 * time.Second)
+	r.Handle("/status", status{}).Methods(http.MethodGet)
+	r.Handle("/submit-code-page", submitCodePage{}).Methods(http.MethodGet)
+	r.Handle("/submit-code", submitCode{zz: zzz}).Methods(http.MethodPost)
+	r.Handle("/list-code", listCode{zz: zzz}).Methods(http.MethodGet, http.MethodPost)
+	r.Handle("/get-code/{uid}", getCode{zz: zzz}).Methods(http.MethodGet)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+type status struct{}
+
+func (s status) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("status: status checker")
+}
+
+type codeDB interface {
+	Store(id, language, code, status string, submittedTime time.Time)
+	Update(id, status, logs string, completedTime time.Time)
+	Get(id string) (language, code, status, logs string, submittedTime, completedTime time.Time)
+}
+
+type codeRecord struct {
+	Language      string
+	Code          string
+	Status        string
+	Logs          string
+	SubmittedTime time.Time
+	CompletedTime time.Time
+}
+
+type cdb struct {
+	Items map[string]codeRecord
+}
+
+func (c cdb) Store(id, language, code, status string, submittedTime time.Time) {
+	c.Items[id] = codeRecord{
+		Language:      language,
+		Code:          code,
+		Status:        status,
+		SubmittedTime: submittedTime,
+	}
+}
+func (c cdb) Update(id, status, logs string, completedTime time.Time) {
+	a := c.Items[id]
+	a.Status = status
+	a.Logs = logs
+	a.CompletedTime = completedTime
+	c.Items[id] = a
+}
+func (c cdb) Get(id string) (language, code, status, logs string, submittedTime, completedTime time.Time) {
+	a := c.Items[id]
+	return a.Language, a.Code, a.Status, a.Logs, a.SubmittedTime, a.CompletedTime
+}
+
+//go:embed templates/*.html
+var viewsFS embed.FS
+
+type submitCodePage struct{}
+
+func (s submitCodePage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("submit code page started")
+	t, err := template.ParseFS(viewsFS, "templates/aaa.html")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("error: %v", err)))
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("error: %v", err)))
+		return
+	}
+}
+
+type submitCode struct {
+	zz cdb
+}
+
+func (s submitCode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("submit code")
+	uid, _ := uuid.NewV4()
+	language := r.FormValue("language")
+	code := r.FormValue("code")
+	status := "submitted"
+	submittedTime := time.Now()
+	s.zz.Store(uid.String(), language, code, status, submittedTime)
+	fmt.Printf("ID record created: %v\n", uid.String())
+	http.Redirect(w, r, "/list-code", http.StatusTemporaryRedirect)
+}
+
+type listCode struct {
+	zz cdb
+}
+
+func (l listCode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	aa := ""
+	for _, z := range l.zz.Items {
+		aa = aa + fmt.Sprintf("%v\n", z)
+	}
+	w.Write([]byte(aa))
+}
+
+type getCode struct {
+	zz cdb
+}
+
+func (g getCode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	uid := r.FormValue("id")
+	singleRecord := g.zz.Items[uid]
+	aa := map[string]string{
+		"code":          singleRecord.Code,
+		"language":      singleRecord.Language,
+		"status":        singleRecord.Status,
+		"logs":          singleRecord.Logs,
+		"submittedTime": singleRecord.SubmittedTime.Format(time.RFC3339),
+		"compltedTime":  singleRecord.CompletedTime.Format(time.RFC3339),
+	}
+	newRaw, err := json.Marshal(aa)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("bad status: %v", err)))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(newRaw)
 }
 
 type codeHandler struct {
@@ -142,15 +245,15 @@ func (c codeHandler) getPodLogs(podName string) string {
 	return logs
 }
 
-func (c codeHandler) createCodeConfigmap() {
+func (c codeHandler) createCodeConfigmap(configmapName, code string) {
 	fmt.Println("start creating test-test config")
 	cConfig := core.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-test",
+			Name: configmapName,
 		},
 		Immutable: boolPtr(true),
 		Data: map[string]string{
-			"code": "import time\ntime.sleep(30)\nprint(\"test\")",
+			"code": code,
 		},
 	}
 	createOpts := metav1.CreateOptions{}
@@ -204,11 +307,11 @@ func (c codeHandler) deleteJob(jobName string) error {
 	return fmt.Errorf("job can still be found - need to investigate")
 }
 
-func (c codeHandler) createJob() *batchv1.Job {
+func (c codeHandler) createJob(jobName, configmapName string) *batchv1.Job {
 	fmt.Println("start creating create job test-test")
 	jConfig := batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-test",
+			Name: jobName,
 		},
 		Spec: batchv1.JobSpec{
 			Parallelism: int32Ptr(1),
@@ -222,11 +325,11 @@ func (c codeHandler) createJob() *batchv1.Job {
 				Spec: core.PodSpec{
 					Containers: []core.Container{
 						{
-							Name: "test",
-							// Image: "python:3.9-alpine",
-							Image:   "nginx:latest",
-							Command: []string{"cat", "/code/code"},
-							// Command: []string{"python", "/code/code"},
+							Name:  "test",
+							Image: "python:3.9-alpine",
+							// Image:   "nginx:latest",
+							// Command: []string{"cat", "/code/code"},
+							Command: []string{"python", "/code/code"},
 							VolumeMounts: []core.VolumeMount{
 								{Name: "miao", ReadOnly: true, MountPath: "/code"},
 							},
@@ -238,7 +341,7 @@ func (c codeHandler) createJob() *batchv1.Job {
 							VolumeSource: core.VolumeSource{
 								ConfigMap: &core.ConfigMapVolumeSource{
 									LocalObjectReference: core.LocalObjectReference{
-										Name: "test-test",
+										Name: configmapName,
 									},
 								},
 							},
@@ -257,7 +360,7 @@ func (c codeHandler) createJob() *batchv1.Job {
 
 	getOpts := metav1.GetOptions{}
 	for i := 0; i < 20; i++ {
-		jj, err := c.client.BatchV1().Jobs(c.workingNamespace).Get(context.TODO(), "test-test", getOpts)
+		jj, err := c.client.BatchV1().Jobs(c.workingNamespace).Get(context.TODO(), jobName, getOpts)
 		if err != nil {
 			fmt.Println("error in getting value for job")
 			time.Sleep(5 * time.Second)
